e6045e5646650987adaea52cc8ea18c368038db9
awangran <ashleero.yin@gmail.com>
2024-07-21 20:16:28 -0500

    editing several works somehow..

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
   });
   const bookRef = useRef()  
   const authorRef = useRef()
+  const typeRef = useRef()
+  const genreRef = useRef()
+  const statusRef = useRef()
+  const ratingRef = useRef()
+
   console.log(books)
 
 
@@ -23,13 +28,19 @@ function App() {
   function handleAdd() {
     const name = bookRef.current.value
     const author = authorRef.current.value
+    const type = typeRef.current.value
+    const genre = Array.from(genreRef.current.selectedOptions, option => option.value)
+    const status = statusRef.current.value
+    const rating = ratingRef.current.value
 
     if (name === '') return
     setBooks(prevBooks => {
-      return [...prevBooks, {id: uuidv4(), name:name, author:author} ]
+      return [...prevBooks, {id: uuidv4(), name:name, author:author, type:type, genre:genre, status:status, rating:rating} ]
     });
     bookRef.current.value = null
     authorRef.current.value = null
+    ratingRef.current.value = null
+    
     }
 
     const deleteItemById = (itemId) => {
@@ -37,9 +48,10 @@ function App() {
       setBooks(newBooks);
     };
 
-    const editName = (itemId, newName, newAuthor) => {
+
+    const editName = (itemId, newName, newAuthor, newType, newGenre, newStatus, newRating) => {
       setBooks(books.map(book =>
-        book.id === itemId ? { ...book, name: newName, author: newAuthor} : book));
+        book.id === itemId ? { ...book, name: newName, author: newAuthor, type: newType, genre: newGenre, status: newStatus, rating: newRating} : book));
       };
     
    
@@ -47,14 +59,61 @@ function App() {
   return (
     <>
     <div>
-      <p className="text-sky-400">Book name: </p>
+      <label className="text-sky-400">Name: </label>
       <input className='border-2 ' ref={bookRef} type='text'></input>
 
-      <p className="text-sky-400">Book author: </p>
+      <label className="text-sky-400">Author: </label>
       <input className='border-2 ' ref={authorRef} type='text'></input>
+
+      <label className="text-sky-400">Type: </label>
+      <select id="type" ref={typeRef}>
+        <option value="book">Book</option>
+        <option value="webnovel">Webnovel</option>
+        <option value="anime">Anime</option>
+        <option value="manga">Manga</option>
+        <option value="manhwa">Manhwa</option>
+        <option value="manhua">Manhua</option>
+        <option value="movie">Movie</option>
+      </select>
+      
+      <label className='text-sky-400'>Genre: </label>
+      <select id="genre" ref={genreRef} multiple>
+        <option value="action">Action</option>
+        <option value="adventure">Adventure</option>
+        <option value="comedy">Comedy</option>
+        <option value="drama">Drama</option>
+        <option value="fantasy">Fantasy</option>
+        <option value="horror">Horror</option>
+        <option value="mystery">Mystery</option>
+        <option value="romance">Romance</option>
+        <option value="sci-fi">Science Fiction</option>
+        <option value="slice-of-life">Slice of Life</option>
+        <option value="sports">Sports</option>
+        <option value="supernatural">Supernatural</option>
+        <option value="thriller">Thriller</option>
+        <option value="mecha">Mecha</option>
+        <option value="historical">Historical</option>
+        <option value="musical">Musical</option>
+        <option value="psychological">Psychological</option>
+      </select>
+
+      <label className='text-sky-400'>Status: </label>
+      <select id="status" ref={statusRef}>
+        <option value="current">Current</option>
+        <option value="completed">Completed</option>
+        <option value="marinating">Marinating</option>
+      </select>
+
+      <label className='text-sky-400'>Rating: </label>
+      <input type='number' min="0" max="10" ref={ratingRef}></input>
+
+
+
+      
 
       <button className='bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded m-2' onClick={handleAdd}>Add book</button>
     </div>
+
     <Library books = {books} deleteItemById = {deleteItemById} editName={editName} />
     <div>{books.length} books in library</div>
     </>
--- src/Book.js
+++ src/Book.js
@@ -18,31 +18,82 @@ export default function Book( {book, deleteBook, deleteItemById, editName} ) {
     const nameRef = useRef()
     const authoreditRef = useRef()
     const settingRef = useRef()
+    const editTypeRef = useRef()
+    const editGenreRef = useRef()
+    const editStatusRef = useRef()
+    const editRatingRef = useRef()
+    
 
     const handleEdit = (event) => {
         const newName = nameRef.current.value
         const newAuthor = authoreditRef.current.value
+        const newType = editTypeRef.current.value
+        const newGenre = Array.from(editGenreRef.current.selectedOptions, option => option.value);
+        const newStatus = editStatusRef.current.value
+        const newRating = editRatingRef.current.value
 
         if (newName === ''|| newAuthor === '') {
             alert('Please dont leave blanks.')
         } else {
-            editName(book.id, newName, newAuthor)
+            editName(book.id, newName, newAuthor, newType, newGenre, newStatus, newRating)
         }
     }
     
   return (
     <div>
-    <p>{book.name} | {book.author}</p>
+    <p>{book.name} | {book.author} | {book.type} | {book.genre} | {book.status} | {book.rating} </p>
       
       <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded m-2" onClick={handleDelete}>delete</button>
       <button onClick={toggleEdit} className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded m-2">edit</button>
 
-      <div ref={settingRef} className='block' >
+      <div ref={settingRef} className='hidden' >
         <label>name:</label>
         <input defaultValue = {book.name} className='border-2' ref={nameRef} type='text'  ></input>
         
         <label>author:</label>
         <input defaultValue = {book.author} className='border-2' ref={authoreditRef} type='text' ></input>
+
+        <label>type:</label>
+        <select id="type" ref={editTypeRef}>
+        <option value="book">Book</option>
+        <option value="webnovel">Webnovel</option>
+        <option value="anime">Anime</option>
+        <option value="manga">Manga</option>
+        <option value="manhwa">Manhwa</option>
+        <option value="manhua">Manhua</option>
+        <option value="movie">Movie</option>
+      </select>
+
+      <label className='text-sky-400'>Genre: </label>
+      <select id="genre" ref={editGenreRef} multiple>
+        <option value="action">Action</option>
+        <option value="adventure">Adventure</option>
+        <option value="comedy">Comedy</option>
+        <option value="drama">Drama</option>
+        <option value="fantasy">Fantasy</option>
+        <option value="horror">Horror</option>
+        <option value="mystery">Mystery</option>
+        <option value="romance">Romance</option>
+        <option value="sci-fi">Science Fiction</option>
+        <option value="slice-of-life">Slice of Life</option>
+        <option value="sports">Sports</option>
+        <option value="supernatural">Supernatural</option>
+        <option value="thriller">Thriller</option>
+        <option value="mecha">Mecha</option>
+        <option value="historical">Historical</option>
+        <option value="musical">Musical</option>
+        <option value="psychological">Psychological</option>
+      </select>
+
+      <label className='text-sky-400'>Status: </label>
+      <select id="status" ref={editStatusRef}>
+        <option value="current">Current</option>
+        <option value="completed">Completed</option>
+        <option value="marinating">Marinating</option>
+      </select>
+
+      <label className='text-sky-400'>Rating: </label>
+      <input type='number' min="0" max="10" ref={editRatingRef}></input>
 
 
         
